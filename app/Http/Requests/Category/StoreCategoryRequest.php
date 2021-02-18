bc1c2a8f8d0169dc02c939bd52d7fbe058f83169
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-18 19:50:28 +0400

    Add form request to define some rules on receiving data to validate them

---
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use App\Http\Requests\FormRequest;
+
+/**
+ * The request to check the incoming parameters to create a category
+ * PHP version >= 7.0
+ *
+ * @category Requests
+ * @package  eShop
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ */
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|string|unique:categories",
+        ];
+    }
+}
