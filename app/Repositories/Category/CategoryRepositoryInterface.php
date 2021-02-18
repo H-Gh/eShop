c3c0e6c6b6f096da6dcb34cfaafda0b27c237ecd
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-18 05:22:02 +0400

    Add repositories for the models

---
+++ app/Repositories/Category/CategoryRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Category;
+
+/**
+ * The repository interface for the category model
+ * PHP version >= 7.0
+ *
+ * @category Repositories
+ * @package  eShop
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ */
+interface CategoryRepositoryInterface
+{
+
+}
---
+++ app/Repositories/Product/ProductRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Product;
+
+/**
+ * The repository interface for the product model
+ * PHP version >= 7.0
+ *
+ * @product Repositories
+ * @package  eShop
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ */
+interface ProductRepositoryInterface
+{
+
+}
