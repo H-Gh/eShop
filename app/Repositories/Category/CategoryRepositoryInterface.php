e7d134313583c3db4a57d5969b89d18fe9dc9bcb
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-18 19:56:48 +0400

    Add some methods to categopry repository to handle the category model

--- app/Repositories/Category/CategoryRepositoryInterface.php
+++ app/Repositories/Category/CategoryRepositoryInterface.php
@@ -2,6 +2,12 @@
 
 namespace App\Repositories\Category;
 
+use App\Exceptions\FailedOperationException;
+use App\Models\Category;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
 /**
  * The repository interface for the category model
  * PHP version >= 7.0
@@ -12,5 +18,41 @@ namespace App\Repositories\Category;
  */
 interface CategoryRepositoryInterface
 {
+    /**
+     * Get all the categories which are active
+     *
+     * @return Category[]|Collection
+     */
+    public function getAllActiveCategories(): Collection|array;
 
+    /**
+     * @param array $properties
+     *
+     * @return Category|Model
+     */
+    public function create(array $properties): Model|Category;
+
+    /**
+     * @param $id
+     *
+     * @return Category|Category[]|Collection|Model|null
+     */
+    public function findByIdOrFail($id): Model|Collection|array|Category|null;
+
+    /**
+     * @param int   $id
+     * @param array $properties
+     *
+     * @return Category|Category[]|Collection|Model|null
+     * @throws FailedOperationException
+     */
+    public function updateById(int $id, array $properties): Model|Collection|array|Category|null;
+
+    /**
+     * @param int $id
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function deleteById(int $id): bool;
 }
