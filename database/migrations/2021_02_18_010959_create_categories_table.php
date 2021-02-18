d3618a3c0ca9c1a33ab3d30e5b795a4577bd2288
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-18 19:58:34 +0400

    Add default value for the status column in migration

--- database/migrations/2021_02_18_010959_create_categories_table.php
+++ database/migrations/2021_02_18_010959_create_categories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +22,7 @@ class CreateCategoriesTable extends Migration
                 $table->id();
                 $table->string("name");
                 $table->string("slug");
-                $table->tinyInteger("status");
+                $table->tinyInteger("status")->default(Category::STATUS["active"]);
                 $table->timestamps();
             });
     }
